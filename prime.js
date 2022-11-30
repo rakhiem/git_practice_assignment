7caa51446d754f12aea301dc12f19d891c8d989d
Abijeet TK <abijeet88@gmail.com>
2022-11-30 12:17:19 +0530

    code_fixed

--- prime.js
+++ prime.js
@@ -1,12 +1,14 @@
+//prime
+let num=14;
 let count =0;
-    for (let i=0;i<=num;i++){
+    for (let i=1;i<=num;i++){
         if (num%i==0){
-            count--;
+            count++;
         }
         
         
     }
-    if (count==1){
+    if (count==2){
         console.log("Yes");
     }
     else {
